b19897c770668939f20e7ddafcec40c8b6da42a7
Rui Salvaterra <rsalvaterra@gmail.com>
2021-02-02 10:02:42 +0000

    kernel/zram: change the default compressor to zstd
    Backport the upstream patch to allow selecting a default compressor and set it
    to zstd, breaking the currently strict dependency on lzo. This allows us to
    completely remove lzo from the build, if nothing else depends on it.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -937,11 +937,12 @@ $(eval $(call KernelPackage,io-schedulers))
 define KernelPackage/zram
   SUBMENU:=$(OTHER_MENU)
   TITLE:=ZRAM
-  DEPENDS:=+kmod-lib-lzo
+  DEPENDS:=+kmod-lib-zstd
   KCONFIG:= \
 	CONFIG_ZSMALLOC \
 	CONFIG_ZRAM \
 	CONFIG_ZRAM_DEBUG=n \
+	CONFIG_ZRAM_DEF_COMP_ZSTD=y \
 	CONFIG_ZRAM_WRITEBACK=n \
 	CONFIG_ZSMALLOC_STAT=n
   FILES:= \
