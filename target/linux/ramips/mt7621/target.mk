83ba3b1733f1c7a4723eb4db3ad3c02dac0cb30a
Vladdrako <vladdrako007@gmail.com>
2021-02-26 18:22:12 +0200

    ramips: move KERNEL_TESTING_PATCHVER to Makefile

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -10,7 +10,6 @@ KERNELNAME:=vmlinux vmlinuz
 # make Kernel/CopyImage use $LINUX_DIR/vmlinuz
 IMAGES_DIR:=../../..
 
-KERNEL_TESTING_PATCHVER:=5.10
 DEFAULT_PACKAGES += wpad-basic-wolfssl
 
 define Target/Description
