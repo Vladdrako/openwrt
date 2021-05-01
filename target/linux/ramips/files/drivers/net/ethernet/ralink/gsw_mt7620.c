9f0cac67e744f5e052eec6b784be47ad236bc016
Michael Pratt <mcpratt@pm.me>
2021-05-01 14:47:01 -0400

    ramips: mt7620: remove unrelated bit change and bump version
    this bit changes PCIe, not anything ethernet related
    
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- target/linux/ramips/files/drivers/net/ethernet/ralink/gsw_mt7620.c
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/gsw_mt7620.c
@@ -90,7 +90,6 @@ static void mt7620_hw_init(struct mt7620_gsw *gsw, int mdio_mode)
 	u32 val;
 	u32 is_BGA = (rt_sysc_r32(0x0c) >> 16) & 1;
 
-	rt_sysc_w32(rt_sysc_r32(SYSC_REG_CFG1) | BIT(8), SYSC_REG_CFG1);
 	mtk_switch_w32(gsw, mtk_switch_r32(gsw, GSW_REG_CKGCR) & ~(0x3 << 4), GSW_REG_CKGCR);
 
 	/* Enable MIB stats */
