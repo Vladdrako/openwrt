df38d30ca15a13bde414655b3b3aaa44a06e7a24
Rui Salvaterra <rsalvaterra@gmail.com>
2021-06-03 11:12:02 +0100

    kernel: bump 5.10 to 5.10.42
    Deleted (reverse-appliable):
    generic/pending-5.10/110-perf-jevents-fix-getting-maximum-number-of-fds.patch
    
    Manually refreshed:
    ramips/patches-5.10/835-asoc-add-mt7620-support.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .124
-LINUX_VERSION-5.10 = .41
+LINUX_VERSION-5.10 = .42
 
 LINUX_KERNEL_HASH-5.4.124 = f7f29dda2b042d7b5986d18274413131cf70e17288c05e9a683df1f46c507d82
-LINUX_KERNEL_HASH-5.10.41 = f604759de80767c4f8bdc500eec730dc161bc914a48bd366b748c176701a6771
+LINUX_KERNEL_HASH-5.10.42 = 8ba027c73bd67b8ded2649a741d2f018db630c1cbc081700d8ffd07ab0d906e5
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
