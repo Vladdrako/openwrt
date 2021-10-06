c46a9408ef6a4fbfae7253a765625d3adbe46270
Vladdrako <vladdrako007@gmail.com>
2021-10-06 08:43:40 +0300

    fix version again

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -12,7 +12,7 @@ LINUX_VERSION-5.14 = .1
 
 LINUX_KERNEL_HASH-5.4.150 = f424a9bbb05007f04c17f96a2e4f041a8001554a9060d2c291606e8a97c62aa2
 LINUX_KERNEL_HASH-5.10.70 = 75736c33711d7f298889ee4981b18774882045323e5c46af9ad3b022082db232
-LINUX_KERNEL_HASH-5.14.0 = skip
+LINUX_KERNEL_HASH-5.14.1 = skip
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
