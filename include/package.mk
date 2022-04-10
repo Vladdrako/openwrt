a7aec37d439b219cc3aa8779a40f2b1590cb2fd5
Vladyslav Andreichykov <vladdrako007@gmail.com>
2022-04-10 15:43:55 +0300

    fix: download: improve handling of invalid local files
    4e19cbc5533: [download: handle possibly invalid local tarballs] added a
    FORCE rule to downloaded files, so that they will be always checked by
    download.pl.
    
    As a side-effect, check-compile will fail, forcing unnecessary package
    rebuilds.
    The check-compile.txt log shows (for libxml2 for example):
      Considering target file '.../dl/libxml2-2.9.12.tar.gz'.
        ...
        prerequisite 'FORCE' of target '.../dl/libxml2-2.9.12.tar.gz' does
        not exist.
        Must remake target '.../dl/libxml2-2.9.12.tar.gz'.
        ...
       Giving up on target file '...libxml2-2.9.12/.prepared_...'.
       Giving up on target file '...libxml2-2.9.12/.configured_...'.
       Giving up on target file '...libxml2-2.9.12/.built'.
       Giving up on target file '...stamp/.libxml2_installed'.
      Giving up on target file '.compile'.
    
    Then the package is rebuilt even if it is not otherwise needed.
    
    To fix this, instead of always forcing the download target to be remade,
    check its hash first: if it matches, then the FORCE is not added.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- include/package.mk
+++ include/package.mk
@@ -184,6 +184,8 @@ define Build/CoreTargets
   $(call Build/Autoclean)
   $(call DefaultTargets)
 
+  $(call check_download_integrity)
+
   download:
 	$(foreach hook,$(Hooks/Download),
 		$(call $(hook))$(sep)
