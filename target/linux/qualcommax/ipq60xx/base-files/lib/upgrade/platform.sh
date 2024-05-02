42b2df3496b8395e034b563ca391dbb125a39ae9
Vladdrako <vladdrako007@gmail.com>
2024-05-02 08:05:03 +0300

    update

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -34,21 +34,14 @@ EOF
 platform_do_upgrade() {
 	case "$(board_name)" in
 	linksys,mr7350)
-	CI_UBIPART="rootfs"
-	boot_part="$(fw_printenv -n boot_part)"
-		if [ -n "$UPGRADE_OPT_CURR_PARTITION" ]; then
-			if [ "$boot_part" -eq "2" ]; then
-				CI_KERNPART="alt_kernel"
-				CI_UBIPART="alt_rootfs"
-			fi
+		boot_part="$(fw_printenv -n boot_part)"
+		if [ "$boot_part" -eq "1" ]; then
+			fw_setenv boot_part 2
+			CI_KERNPART="alt_kernel"
+			CI_UBIPART="alt_rootfs"
 		else
-			if [ "$boot_part" -eq "1" ]; then
-				fw_setenv boot_part 2
-				CI_KERNPART="alt_kernel"
-				CI_UBIPART="alt_rootfs"
-			else
-				fw_setenv boot_part 1
-			fi
+			fw_setenv boot_part 1
+			CI_UBIPART="rootfs"
 		fi
 		fw_setenv boot_part_ready 3
 		fw_setenv auto_recovery yes
